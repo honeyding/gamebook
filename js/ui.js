f90f43eae404b4ed77719a46c1db7cadce497d07
honeyding <team.honeyding@gmail.com>
2019-06-16 16:04:48 +0900

    modi file

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,26 @@
 $(function(){
 
+	$('.btn-nav').on('click', function(){
+		// $('.nav').show().animate({
+		// 	top:0,
+		// 	right:0,
+		// 	width:'100%',
+		// 	height:'100%'
+		// });
+		$('.nav').fadeIn();
+	});
+	$('.btn-close').on('click', function(){
+		// $('.nav').animate({
+		// 	top:'70px',
+		// 	right:'110px',
+		// 	width:0,
+		// 	height:0
+		// }, function(){
+		// 	$('.nav').hide()
+		// });
+		$('.nav').fadeOut();
+	});
+
 	$('.book-list .slide-area').bxSlider({
 		pager: false,
 		onSliderLoad: function(){
