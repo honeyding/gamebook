6bf6cc66c0766153fc6aae2a35998b388155f98a
honeyding <team.honeyding@gmail.com>
2019-06-25 21:44:53 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -10,7 +10,7 @@ $(function(){
 		}
 	});
 
-	$('.book-list .slide-area').bxSlider({
+	pcOption = {
 		pager: false,
 		touchEnabled : (navigator.maxTouchPoints > 0),
 		onSliderLoad: function(){
@@ -33,7 +33,37 @@ $(function(){
 					break;
 			}
 		}
-	});
+	}
+
+	mobileOption = {
+		pager: false,
+		onSliderLoad: function(){
+			$('.book-item[aria-hidden=false]').find('.info .title, .info .provide').show();
+		},
+		onSlideBefore: function($slideElement, oldIndex, newIndex){
+			$('.info .title, .info .provide').fadeOut();
+			$slideElement.find('.info .title, .info .provide').fadeIn();
+
+			var num = newIndex%3;
+			switch(num){
+				case 0:
+					$('.book-wrap').css('background','#f6dc64');
+					break;
+				case 1:
+					$('.book-wrap').css('background','#efe9e1');
+					break;
+				case 2:
+					$('.book-wrap').css('background','#8dd5a8');
+					break;
+			}
+		}
+	}
+
+	if($(window).width() > 768){
+		$('.book-list .slide-area').bxSlider(pcOption);
+	} else{
+		$('.book-list .slide-area').bxSlider(mobileOption);
+	}
 
 	$(window).resize(function() {
 	    itemHeight();
